1ac6945c96e8b4e2a077440710180aafccde84fc
pheralb <pablohdez4340@gmail.com>
2022-09-05 00:27:27 +0100

    ⚒️ Add active link.

--- src/components/sidebar/item.tsx
+++ src/components/sidebar/item.tsx
@@ -1,4 +1,6 @@
 import { Box, Tooltip, useColorModeValue } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import CustomLink from "@/components/link";
 interface SidebarItemProps {
   icon: React.ReactNode;
   description: string;
@@ -6,22 +8,28 @@ interface SidebarItemProps {
 }
 
 const SidebarItem = (props: SidebarItemProps) => {
+  const { pathname } = useRouter();
+  const isActive = pathname === props.href;
   const bg = useColorModeValue("tooltip.light", "tooltip.dark");
+  const colorActive = useColorModeValue("gray.900", "gray.100");
+
   return (
-    <Tooltip bg={bg} label={props.description} hasArrow placement='auto'>
-      <Box
-        cursor="pointer"
-        p="2"
-        mb="4"
-        color="gray.500"
-        _hover={{
-          color: "gray.100",
-          transform: "translateY(-1px)",
-          transition: "transform .3s ease-in-out",
-        }}
-      >
-        {props.icon}
-      </Box>
+    <Tooltip bg={bg} label={props.description} hasArrow placement="auto">
+      <CustomLink href={props.href}>
+        <Box
+          cursor="pointer"
+          p="2"
+          mb="4"
+          color={isActive ? colorActive : "gray.500"}
+          _hover={{
+            color: "gray.100",
+            transform: "translateY(-1px)",
+            transition: "transform .3s ease-in-out",
+          }}
+        >
+          {props.icon}
+        </Box>
+      </CustomLink>
     </Tooltip>
   );
 };
