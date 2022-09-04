beb6128634cfa7c13e24da2503c9f5e6a499c416
pheralb <pablohdez4340@gmail.com>
2022-09-04 19:36:01 +0100

    ⚒️ Add tooltip for sidebar item.

--- src/components/sidebar/item.tsx
+++ src/components/sidebar/item.tsx
@@ -1,6 +1,4 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
-
+import { Box, Tooltip, useColorModeValue } from "@chakra-ui/react";
 interface SidebarItemProps {
   icon: React.ReactNode;
   description: string;
@@ -8,20 +6,23 @@ interface SidebarItemProps {
 }
 
 const SidebarItem = (props: SidebarItemProps) => {
+  const bg = useColorModeValue("tooltip.light", "tooltip.dark");
   return (
-    <Box
-      cursor="pointer"
-      p="2"
-      mb="4"
-      color="gray.500"
-      _hover={{
-        color: "gray.100",
-        transform: "translateY(-1px)",
-        transition: "transform .3s ease-in-out",
-      }}
-    >
-      {props.icon}
-    </Box>
+    <Tooltip bg={bg} label={props.description} hasArrow placement='auto'>
+      <Box
+        cursor="pointer"
+        p="2"
+        mb="4"
+        color="gray.500"
+        _hover={{
+          color: "gray.100",
+          transform: "translateY(-1px)",
+          transition: "transform .3s ease-in-out",
+        }}
+      >
+        {props.icon}
+      </Box>
+    </Tooltip>
   );
 };
 
