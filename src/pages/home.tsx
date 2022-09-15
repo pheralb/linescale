c3fc4ddcd1f0c96540c8c1d44d28a08e9ff32051
pheralb <pablohdez4340@gmail.com>
2022-09-15 21:58:42 +0100

    ⚒️ Add open & read file.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { ipcRenderer } from "electron";
+import EditorComponent from "@/components/editor";
+
 const Home: React.FC = () => {
+  const [value, setValue] = useState("hello world");
+
+  ipcRenderer.on("file", (event, content) => {
+    setValue(content);
+  });
+
   return (
     <>
-      <h1>Hello World 🥳</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <EditorComponent
+        value={value}
+        height="90vh"
+        language="typescript"
+        minimap={false}
+        onChange={(value) => setValue(value ?? "")}
+      />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
